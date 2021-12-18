cb92c6b9fa5ceb216411db3d931484b634e56a47
z003w4hv <rohit.pal@siemens.com>
2021-12-18 18:39:39 +0530

    feat: establishing db connection

---
+++ src/Database/Configuration.ts
@@ -0,0 +1,19 @@
+import { Knex } from "knex";
+import dbConnection from "./Connection";
+/**
+ * Using singleton pattern so that we can have only one instance of db and we don't need to create instance again and again.
+ */
+class DBConfiguration {
+    public dbConnector: Knex;
+    private static dbInstance: DBConfiguration;
+    constructor() {
+        this.dbConnector = dbConnection.connectionToDB();
+    }
+    public static DBInstance(): DBConfiguration {
+        if (!DBConfiguration.dbInstance) {
+            return new DBConfiguration;
+        }
+        return DBConfiguration.dbInstance;
+    }
+}
+export default DBConfiguration;
