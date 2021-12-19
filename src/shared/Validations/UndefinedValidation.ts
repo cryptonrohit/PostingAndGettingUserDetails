c70eded956e7491df493dd4a1a9c45cfad350c82
z003w4hv <rohit.pal@siemens.com>
2021-12-19 11:13:19 +0530

    feat: changed undefined validation way

--- src/shared/Validations/UndefinedValidation.ts
+++ src/shared/Validations/UndefinedValidation.ts
@@ -4,5 +4,8 @@
  * @returns boolean. Whether the data entered by user is undefined or not.
  */
 export function undefinedValidation(data: string): boolean {
-    return data === undefined ? false : true;
+    if (!data) {
+        return false;
+    }
+    return true;
 }
