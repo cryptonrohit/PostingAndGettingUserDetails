331712319eac0db005e00d2fbcc994881ed8b2bc
z003w4hv <rohit.pal@siemens.com>
2021-12-19 13:58:12 +0530

    fix: improved email validation

--- src/shared/Validations/EmailValidation.ts
+++ src/shared/Validations/EmailValidation.ts
@@ -2,7 +2,7 @@
 /**
  * 
  * @param data email id of the user
- * 1. \w - matches any word character till @
+ * 1. [a-zA-Z0-9_\.] - string containing any characters from a-z or A-Z including . and including underscore
  * 2. @ - there should be @ sign
  * 3. [a-zA-Z_] - string containing any characters from a-z or A-Z including underscore(_)
  * 4. \. - there should be a dot after the string
@@ -10,6 +10,6 @@
  * @returns boolean. Whether email id is valid or not.
  */
 export function emailValidation(data: string): boolean {
-    const emailRegex = /^\w+@[a-zA-Z_]+\.[a-zA-Z]{2,3}$/g;
+    const emailRegex = /^[a-zA-Z0-9_\.]+@[a-zA-Z_]+\.[a-zA-Z]{2,3}$/g;
     return data.match(emailRegex) ? true : false;
 }
