3757e4296b06905feba16a5f61eeaf76e200f457
z003w4hv <rohit.pal@siemens.com>
2021-12-19 14:08:34 +0530

    feat: added TOKEN_SECRET as constant

--- src/shared/Constants.ts
+++ src/shared/Constants.ts
@@ -1,3 +1,4 @@
 export const protocols = ["http:", "https:"];
 export const panFourthCharInclusions = ["C","P","H","F","A","T","B","L","J","G"];
 export const genders = ["male", "female", "transgender"];
+export const TOKEN_SECRET = "7ebb7ff70a0f25ff42c75e9a283f1fc1d5f163b80a6d18e941254a61b10edb96e694d2b26e5935ee6c0683961cac850416ed788fd4c908675bb3fabdf97dbe26";
