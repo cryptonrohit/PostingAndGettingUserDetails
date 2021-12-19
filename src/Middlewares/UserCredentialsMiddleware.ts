f925629593474b7aa2395162fa6c54e2ac8d775a
z003w4hv <rohit.pal@siemens.com>
2021-12-19 13:44:10 +0530

    fix: issue in credentials

--- src/Middlewares/UserCredentialsMiddleware.ts
+++ src/Middlewares/UserCredentialsMiddleware.ts
@@ -3,11 +3,11 @@ import { NextFunction, Request, Response } from "express";
 class UserCredentialsMiddleware {
     validate(req: Request, res: Response, next: NextFunction) {
         const { username, password } = req.body;
-        if (!username && !password) {
+        if (!username || !password) {
             return res.status(400).send("Either of username or password is undefined");
         }
         // Validating user and password with dummy data.
-        if (username !== "testuser" && password !== "testpassword") {
+        if (username !== "admin" || password !== "root") {
             return res.status(400).send("Invalid username or password");
         }
         next();
