af9dfd3e5f75aa3e2e142b2717d41bf35cf81cfd
z003w4hv <rohit.pal@siemens.com>
2021-12-19 16:43:41 +0530

    fix: improper file path

--- src/Database/TableCreation/UserDataTable.ts
+++ src/Database/TableCreation/UserDataTable.ts
@@ -1,5 +1,5 @@
 import { Knex } from "knex";
-import { USER_DATA } from "../TableNames/tableNames";
+import { USER_DATA } from "../TableNames/TableNames";
 
 class UserDataTable {
     async create(dbConnector: Knex) {
