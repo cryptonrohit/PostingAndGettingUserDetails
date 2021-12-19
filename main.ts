203803370d461a319a85e621fb71acf4575e5638
z003w4hv <rohit.pal@siemens.com>
2021-12-19 17:38:10 +0530

    feat: chnaged port

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
-const port =  3456;
+const port = 80;
 
 app.listen(port, () => {
     console.log(`listening on port ${port}......`);
